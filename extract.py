3ea07ed7187a174e7985a8d9c5252effed69d57c
Kotbenek <dominik.piatkowski@protonmail.com>
2022-06-05 15:31:56 +0200

    Modularized existing scripts

--- extract.py
+++ extract.py
@@ -3,33 +3,45 @@ import shutil
 from os import listdir
 from os.path import exists
 
-etl_dirs = ["ETL/ETL1", "ETL/ETL2", "ETL/ETL3", "ETL/ETL4", "ETL/ETL5", "ETL/ETL6", "ETL/ETL7", "ETL/ETL8B", "ETL/ETL8G", "ETL/ETL9B", "ETL/ETL9G"]
-files = []
+def check_if_dir_exists():
+    if exists("extracted"):
+        print("Directory 'extracted' already exists.")
+        quit()
 
-if exists("extracted"):
-    print("Directory 'extracted' already exists.")
-    quit()
+def create_file_list(etl_dirs, files):
+    for dir in etl_dirs:
+        for f in listdir(dir):
+            if f.startswith("ETL") and not f.endswith("INFO"):
+                files.append(dir + "/" + f)
 
-for dir in etl_dirs:
-    for f in listdir(dir):
-        if f.startswith("ETL") and not f.endswith("INFO"):
-            files.append(dir + "/" + f)
+def extract_files(files):
+    for f in files:
+        print("Extracting " + f + "...")
+        os.system("python3 unpack.py " + f)
+        print("Extracted " + f)
 
-for f in files:
-    print("Extracting " + f + "...")
-    os.system("python3 unpack.py " + f)
-    print("Extracted " + f)
+def create_extracted_file_list(etl_dirs, files):
+    files.clear()
+    for dir in etl_dirs:
+        for f in listdir(dir):
+            if f.endswith(".png") or f.endswith(".txt") or f.endswith(".csv"):
+                files.append(dir + "/" + f)
 
-files = []
-for dir in etl_dirs:
-    for f in listdir(dir):
-        if f.endswith(".png") or f.endswith(".txt") or f.endswith(".csv"):
-            files.append(dir + "/" + f)
+def move_extracted_files_to_dedicated_dir(files):
+    os.mkdir("extracted")
 
-os.mkdir("extracted")
+    print("Moving files to 'extracted' directory...")
 
-print("Moving files to 'extracted' directory...")
+    for f in files:
+        shutil.move(f, "extracted")
 
-for f in files:
-    shutil.move(f, "extracted")
+if __name__ == "__main__":
+    etl_dirs = ["ETL/ETL1", "ETL/ETL2", "ETL/ETL3", "ETL/ETL4", "ETL/ETL5", "ETL/ETL6", "ETL/ETL7", "ETL/ETL8B", "ETL/ETL8G", "ETL/ETL9B", "ETL/ETL9G"]
+    files = []
+
+    check_if_dir_exists()
+    create_file_list(etl_dirs, files)
+    extract_files(files)
+    create_extracted_file_list(etl_dirs, files)
+    move_extracted_files_to_dedicated_dir(files)
 
--- extract_hiragana.py
+++ extract_hiragana.py
@@ -2,24 +2,30 @@ import os
 from os.path import exists
 from PIL import Image
 
-files = ["extracted/ETL8B2C1_00", "extracted/ETL8B2C1_01", "extracted/ETL8B2C1_02", "extracted/ETL8B2C1_03", "extracted/ETL8B2C1_04", "extracted/ETL8B2C1_05"]
+def check_if_dir_exists():
+    if exists("extracted_hiragana"):
+        print("Directory 'extracted_hiragana' already exists.")
+        quit()
 
-if exists("extracted_hiragana"):
-    print("Directory 'extracted_hiragana' already exists.")
-    quit()
+def extract_hiragana(files):
+    os.mkdir("extracted_hiragana")
 
-os.mkdir("extracted_hiragana")
+    i = 0
 
-i = 0
+    for file in files:
+        with Image.open(file + ".png") as image:
+            for y in range(40):
+                for x in range(50):
+                    width, height = image.size
+                    sample_width = width // 50
+                    sample_height = height // 40
+                    sample = image.crop((x * sample_width, y * sample_height, (x + 1) * sample_width, (y + 1) * sample_height))
+                    sample.save("extracted_hiragana/" + format(i, "05d") + ".png");
+                    i = i + 1
 
-for file in files:
-    with Image.open(file + ".png") as image:
-        for y in range(40):
-            for x in range(50):
-                width, height = image.size
-                sample_width = width // 50
-                sample_height = height // 40
-                sample = image.crop((x * sample_width, y * sample_height, (x + 1) * sample_width, (y + 1) * sample_height))
-                sample.save("extracted_hiragana/" + format(i, "05d") + ".png");
-                i = i + 1
+if __name__ == "__main__":
+    files = ["extracted/ETL8B2C1_00", "extracted/ETL8B2C1_01", "extracted/ETL8B2C1_02", "extracted/ETL8B2C1_03", "extracted/ETL8B2C1_04", "extracted/ETL8B2C1_05"]
+
+    check_if_dir_exists()
+    extract_hiragana(files)
 
